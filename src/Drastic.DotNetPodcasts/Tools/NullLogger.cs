e90962cd31c94c00905a843034ef7b37baa209a1
Timothy Miller <innerlogic4321@gmail.com>
2022-06-20 18:58:23 -0400

    Add more stuff

---
+++ src/Drastic.DotNetPodcasts/Tools/NullLogger.cs
@@ -0,0 +1,31 @@
+﻿// <copyright file="NullLogger.cs" company="Drastic Actions">
+// Copyright (c) Drastic Actions. All rights reserved.
+// </copyright>
+
+using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.Extensions.Logging;
+
+namespace Drastic.DotNetPodcasts
+{
+    public class NullLoggerFactory : ILoggerFactory
+    {
+        public void AddProvider(ILoggerProvider provider) { }
+
+        public ILogger CreateLogger(string categoryName) => NullLogger.Instance;
+
+        public void Dispose() { }
+    }
+
+    public class NullLogger<T> : ILogger<T>, IDisposable
+    {
+        public IDisposable BeginScope<TState>(TState state) => this;
+
+        public void Dispose() { }
+
+        public bool IsEnabled(LogLevel logLevel) => false;
+
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception?, string> formatter)
+        {
+        }
+    }
+}
