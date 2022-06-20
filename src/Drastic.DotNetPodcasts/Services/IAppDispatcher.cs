6aaedf74bd72811b752e2686cbc4422c3e74c1fb
Timothy Miller <innerlogic4321@gmail.com>
2022-06-20 13:37:30 -0400

    Initial Commit

---
+++ src/Drastic.DotNetPodcasts/Services/IAppDispatcher.cs
@@ -0,0 +1,11 @@
+﻿// <copyright file="IAppDispatcher.cs" company="Drastic Actions">
+// Copyright (c) Drastic Actions. All rights reserved.
+// </copyright>
+
+namespace Drastic.DotNetPodcasts
+{
+    public interface IAppDispatcher
+    {
+        bool Dispatch(Action action);
+    }
+}
