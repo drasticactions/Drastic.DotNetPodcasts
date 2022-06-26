5c01d4a69d85877cb8e90d3b815365eec203240b
Tim Miller <innerlogic4321@gmail.com>
2022-06-25 23:07:59 -0400

    Add items

--- src/Drastic.DotNetPodcasts/ViewModels/PodcastShowItemListViewModel.cs
+++ src/Drastic.DotNetPodcasts/ViewModels/PodcastShowItemListViewModel.cs
@@ -2,6 +2,9 @@
 // Copyright (c) Drastic Actions. All rights reserved.
 // </copyright>
 
+using System.Collections.ObjectModel;
+using DrasticMedia.Core.Model;
+
 namespace Drastic.DotNetPodcasts
 {
     /// <summary>
@@ -22,20 +25,45 @@ namespace Drastic.DotNetPodcasts
             this.Library.RemoveMediaItem += this.Library_RemoveMediaItem;
         }
 
+        /// <summary>
+        /// Gets the list of podcasts.
+        /// </summary>
+        public ObservableCollection<PodcastShowItem> Shows { get; } = new ObservableCollection<PodcastShowItem>();
+
         private void Library_RemoveMediaItem(object? sender, DrasticMedia.Core.Library.RemoveMediaItemEventArgs e)
         {
+            if (e.MediaItem is PodcastShowItem item)
+            {
+                this.Shows.Remove(item);
+            }
         }
 
         private void Library_NewMediaItemError(object? sender, DrasticMedia.Core.Library.NewMediaItemErrorEventArgs e)
         {
+            // TODO: Show Error.
         }
 
         private void Library_UpdateMediaItemAdded(object? sender, DrasticMedia.Core.Library.UpdateMediaItemEventArgs e)
         {
+            if (e.MediaItem is PodcastShowItem item)
+            {
+                var pod = this.Shows.FirstOrDefault(n => n.Id == item.Id);
+                if (pod is null)
+                {
+                    return;
+                }
+
+                this.Shows.Insert(this.Shows.IndexOf(pod), item);
+                this.Shows.Remove(pod);
+            }
         }
 
         private void Library_NewMediaItemAdded(object? sender, DrasticMedia.Core.Library.NewMediaItemEventArgs e)
         {
+            if (e.MediaItem is PodcastShowItem item)
+            {
+                this.Shows.Add(item);
+            }
         }
     }
 }
